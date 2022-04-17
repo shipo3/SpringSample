ff8225831ed3042709ec4a38b4f956a9a7b8cea8
shipo3 <yfb13555@nifty.com>
2022-04-17 20:50:31 +0900

    Controllerの設定追加

--- main/java/com/example/demo/controller/HelloWorldController.java
+++ main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -10,4 +13,16 @@ public class HelloWorldController {
 	return "index";
     }
 
+    @PostMapping("/index")
+    public String postRequest1(@RequestParam("text1") String str1, Model model) {
+
+	model.addAttribute("name", str1);
+	return "hello/response";
+    }
+
+    public String postRequest2(@RequestParam("text2") String str2, Model model) {
+
+	model.addAttribute("message", str2);
+	return "hello/response";
+    }
 }
