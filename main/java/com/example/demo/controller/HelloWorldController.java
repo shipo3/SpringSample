b0a865b0af135b2009d6f5906e7e02a96f69fd01
shipo3 <yfb13555@nifty.com>
2022-04-22 02:29:53 +0900

    サービスクラスの追加（未完成）

--- main/java/com/example/demo/controller/HelloWorldController.java
+++ main/java/com/example/demo/controller/HelloWorldController.java
@@ -8,6 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
+
+    /*
+     * private HelloWorldService helloWorldService;
+     * 
+     * 
+     * public HelloWorldController(HelloWorldService helloWorldService) {
+     * this.helloWorldService = helloWorldService; }
+     */
+
     @GetMapping("/index")
     public String getHello() {
 	return "index";
@@ -15,11 +24,13 @@ public class HelloWorldController {
 
     @PostMapping("/index")
 
-    public String postRequest1(@RequestParam("text1") String str1, Model model1, @RequestParam("text2") String str2,
-	    Model model2) {
+    public String postRequest(@RequestParam("text1") String str1, @RequestParam("text2") String str2, Model model1,
+	    Model model2, Model model3) {
 
 	model1.addAttribute("name", str1);
 	model2.addAttribute("message", str2);
+//	model3.addAttribute("response", helloWorldService.responseMessage(str2));
+
 	return "hello/response";
     }
 
